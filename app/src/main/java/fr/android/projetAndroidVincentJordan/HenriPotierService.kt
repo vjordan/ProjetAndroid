dc83cdb6920da1644dbba3840286982b59d590f4
vjordan <vincent.jordan@outlook.fr>
2018-11-25 21:14:26 +0100

    modif lié à retrofit

--- app/src/main/java/fr/android/projetAndroidVincentJordan/HenriPotierService.kt
+++ app/src/main/java/fr/android/projetAndroidVincentJordan/HenriPotierService.kt
@@ -1,5 +1,13 @@
 package fr.android.projetAndroidVincentJordan
 
+import retrofit2.Call
+import retrofit2.http.GET
+
 interface HenriPotierService {
+
     // TODO Method GET books which return a List<Book>
+    interface Api {
+        @GET("books")
+        fun listBooks(): Call<List<Book>>
+    }
 }
--- app/src/main/java/fr/android/projetAndroidVincentJordan/LibraryActivity.kt
+++ app/src/main/java/fr/android/projetAndroidVincentJordan/LibraryActivity.kt
@@ -2,6 +2,12 @@ package fr.android.projetAndroidVincentJordan
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import timber.log.Timber
+import retrofit2.Retrofit.*
+import retrofit2.converter.gson.GsonConverterFactory
 
 class LibraryActivity : AppCompatActivity() {
 
@@ -10,19 +16,34 @@ class LibraryActivity : AppCompatActivity() {
         setContentView(R.layout.activity_library)
 
         // Plant logger cf. Android Timber
-        // Timber.plant(new Timber.DebugTree());
+        Timber.plant(Timber.DebugTree())
+
 
         // TODO build Retrofit
+        val retrofit = Builder()
+                .baseUrl("http://henri-potier.xebia.fr/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
 
         // TODO create a service
+        val api = retrofit.create(HenriPotierService.Api::class.java)
 
         // TODO listBooks()
+        api.listBooks()
 
         // TODO enqueue call and display book title
+        api.listBooks().enqueue(object : Callback<List<Book>> {
+            override fun onFailure(call: Call<List<Book>>, t: Throwable) {
+                Timber.e(t)
+            }
 
-        // TODO log books
+            override fun onResponse(call: Call<List<Book>>,
+                                    response: Response<List<Book>>) {
+                // TODO log books
+                response.body()?.forEach { Timber.i(it.toString()) }
+            }
+        })
 
         // TODO display book as a list
     }
-
 }
